907f7ea2432440e5a89ddf60984792b150dfff7a
Jacob Fisher <jacobfisher18@gmail.com>
2017-05-16 16:05:58 -0700

    added slots for homwork boxes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
 #include <iostream>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+//global variables
+int homeworks[8] = {};
+
+void printHomeworks() {
+    for (int i = 0; i < 8; i++) {
+        std::cout << homeworks[i] << std::endl;
+    }
+}
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -15,7 +21,58 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void updateOverall(Ui::MainWindow* ui) {
+    double overall = 0;
+    for (int i = 0; i < 8; i++) {
+        overall+=homeworks[i];
+    }
+
+    overall /= 8;
+
+    ui->overalllabel->setText(QString::number(overall));
+}
+
+void hwboxChanged(int index, int& value, Ui::MainWindow* ui) {
+    homeworks[index] = value;
+    updateOverall(ui);
+}
+
 void MainWindow::on_hw1box_valueChanged(int arg1)
 {
-    std::cout << arg1 << std::endl;
+    hwboxChanged(0, arg1, ui);
+}
+
+void MainWindow::on_hw2box_valueChanged(int arg1)
+{
+    hwboxChanged(1, arg1, ui);
+}
+
+void MainWindow::on_hw3box_valueChanged(int arg1)
+{
+    hwboxChanged(2, arg1, ui);
+}
+
+void MainWindow::on_hw4box_valueChanged(int arg1)
+{
+    hwboxChanged(3, arg1, ui);
+}
+
+void MainWindow::on_hw5box_valueChanged(int arg1)
+{
+    hwboxChanged(4, arg1, ui);
+}
+
+void MainWindow::on_hw6box_valueChanged(int arg1)
+{
+    hwboxChanged(5, arg1, ui);
+}
+
+void MainWindow::on_hw7box_valueChanged(int arg1)
+{
+    hwboxChanged(6, arg1, ui);
+}
+
+void MainWindow::on_hw8box_valueChanged(int arg1)
+{
+    hwboxChanged(7, arg1, ui);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,6 +18,20 @@ public:
 private slots:
     void on_hw1box_valueChanged(int arg1);
 
+    void on_hw2box_valueChanged(int arg1);
+
+    void on_hw3box_valueChanged(int arg1);
+
+    void on_hw4box_valueChanged(int arg1);
+
+    void on_hw5box_valueChanged(int arg1);
+
+    void on_hw6box_valueChanged(int arg1);
+
+    void on_hw7box_valueChanged(int arg1);
+
+    void on_hw8box_valueChanged(int arg1);
+
 private:
     Ui::MainWindow *ui;
 };
